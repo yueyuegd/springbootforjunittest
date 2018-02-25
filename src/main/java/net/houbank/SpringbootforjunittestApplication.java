21edc15eaff94b3ac6af8032abe50a6f3abd0f1e
Zhang Yue <zhangyue01@houbank.cn>
2018-02-25 19:11:49 +0800

    Initial commit

---
+++ src/main/java/net/houbank/SpringbootforjunittestApplication.java
@@ -0,0 +1,12 @@
+package net.houbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootforjunittestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootforjunittestApplication.class, args);
+	}
+}
---
+++ src/test/java/net/houbank/SpringbootforjunittestApplicationTests.java
@@ -0,0 +1,16 @@
+package net.houbank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootforjunittestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
